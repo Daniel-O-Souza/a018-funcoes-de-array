1c8a16361592e6c1976459c27b15207d92372bef
Daniel <daniel.sprcp@gmail.com>
2022-08-25 16:50:03 -0300

    Exercicio 2 finalizado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,20 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+// EXERCÍCIO 1:
+
+// A:
+
+const maiuscula = (objetos) =>{
+
+    return{
+
+        nome: "ASTRODEV",
+        profissao: "Dev das estrelas".toUpperCase(),
+        username: "atrodev_labenu".toUpperCase(),
+        senha: "melhorDeTodos".toUpperCase()
+    }
+}
+
+console.log(maiuscula)
---
+++ exercicio2/script.js
@@ -0,0 +1,16 @@
+// EXERCÍCIO 2:
+
+const numeros = [2, 10, 15, 321, 44, 50, 99, 115]
+
+// A:
+
+const triplos = numeros.map(numero => numero*3)
+
+console.log(triplos)
+
+// B:
+
+const metades = numeros.map(numero => numero/2)
+
+console.log(metades)
+
